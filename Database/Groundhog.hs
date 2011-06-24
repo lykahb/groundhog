7830ab822d3eacd92d7a1d2e290e6e593bf0a98a
Boris Lykah <lykahb@gmail.com>
2011-06-24 13:59:42 +0300

    Example modified

--- Database/Groundhog.hs
+++ Database/Groundhog.hs
@@ -5,34 +5,34 @@
 -- @
 --{-\# LANGUAGE GADTs, TypeFamilies, TemplateHaskell \#-}
 --import Control.Monad.IO.Class(liftIO)
---import Database.Groundhog.Sqlite
 --import Database.Groundhog.TH
--- 
+--import Database.Groundhog.Sqlite
+--
 --data Customer a = Customer {customerName :: String, details :: a} deriving Show
---data Item = ProductItem {productName :: String, quantity :: Int, customer :: Customer String}
---          | ServiceItem {serviceName :: String, deliveryAddress :: String, servicePrice :: Int}
+--data Item = Product {productName :: String, quantity :: Int, customer :: Customer String}
+--          | Service {serviceName :: String, deliveryAddress :: String, servicePrice :: Int}
 --     deriving Show
--- 
+--
 --'deriveEntity' ''Customer $ Just $ do
 --  setConstructor 'Customer $ do
 --    setConstraints [(\"NameConstraint\", [\"customerName\"])]
 --deriveEntity ''Item Nothing
--- 
+--
 --main = withSqliteConn \":memory:\" $ runSqliteConn $ do
 --  -- Customer is also migrated because Item contains it
 --  'runMigration' 'silentMigrationLogger' $ 'migrate' (undefined :: Item)
 --  let john = Customer \"John Doe\" \"Phone: 01234567\"
 --  johnKey <- 'insert' john
 --  -- John is inserted only once because of the name constraint
---  insert $ ProductItem \"Apples\" 5 john
---  insert $ ProductItem \"Melon\" 2 john
---  insert $ ServiceItem \"Taxi\" \"Elm Street\" 50
---  insert $ ProductItem \"Melon\" 6 (Customer \"Jack Smith\" \"Don't let him pay by check\")
+--  insert $ Product \"Apples\" 5 john
+--  insert $ Product \"Melon\" 2 john
+--  insert $ Service \"Taxi\" \"Elm Street\" 50
+--  insert $ Product \"Melon\" 6 (Customer \"Jack Smith\" \"Don't let him pay by check\")
 --  -- bonus melon for all large melon orders
 --  'update' [QuantityField '=.' toArith QuantityField + 1] (ProductNameField '==.' \"Melon\" '&&.' QuantityField '>.' (5 :: Int))
 --  productsForJohn <- 'select' (CustomerField ==. johnKey) [] 0 0
 --  liftIO $ putStrLn $ \"Products for John: \" ++ show productsForJohn
---  -- let's check bonus
+--  -- check bonus
 --  melon <- select (ProductNameField ==. \"Melon\") ['Desc' QuantityField] 0 0
 --  liftIO $ putStrLn $ \"Melon orders: \" ++ show melon
 -- @
