bb51b8f575eec180b154e4641bbae1626de07533
Boris Lykah <lykahb at gmail.com>
2012-09-05 20:45:30 +0300

    enhance examples

--- examples/basic.hs
+++ examples/basic.hs
@@ -1,12 +1,10 @@
 {-# LANGUAGE GADTs, TypeFamilies, TemplateHaskell, QuasiQuotes, FlexibleInstances #-}
-import Control.Monad.IO.Class(liftIO)
+import Control.Monad.IO.Class (liftIO)
 import Database.Groundhog.TH
 import Database.Groundhog.Sqlite
 
-data Customer a = Customer {customerName :: String, details :: a} deriving Show
-data Item = Product {productName :: String, quantity :: Int, customer :: Customer String}
-          | Service {serviceName :: String, deliveryAddress :: String, servicePrice :: Int}
-     deriving Show
+data Customer a = Customer {customerName :: String, remark :: a} deriving Show
+data Product = Product {productName :: String, quantity :: Int, customer :: Customer String} deriving Show
 
 mkPersist suffixNamingStyle [groundhog|
 - entity: Customer
@@ -15,20 +13,19 @@ mkPersist suffixNamingStyle [groundhog|
       constraints:
         - name: NameConstraint
           fields: [customerName]
-- entity: Item
+- entity: Product
 |]
 
 main = withSqliteConn ":memory:" $ runSqliteConn $ do
-  -- Customer is also migrated because Item contains it
-  runMigration defaultMigrationLogger $ migrate (undefined :: Item)
+  -- Customer is also migrated because Product references it
+  runMigration defaultMigrationLogger $ migrate (undefined :: Product)
   let john = Customer "John Doe" "Phone: 01234567"
   johnKey <- insert john
   -- John is inserted only once because of the name constraint
   insert $ Product "Apples" 5 john
   insert $ Product "Melon" 2 john
-  insert $ Service "Taxi" "Elm Street" 50
   insert $ Product "Melon" 6 (Customer "Jack Smith" "Don't let him pay by check")
-  -- bonus melon for all large melon orders
+  -- bonus melon for all large melon orders. The values used in expressions should have known type, so literal 5 is annotated.
   update [QuantityField =. toArith QuantityField + 1] (ProductNameField ==. "Melon" &&. QuantityField >. (5 :: Int))
   productsForJohn <- select (CustomerField ==. johnKey) [] 0 0
   liftIO $ putStrLn $ "Products for John: " ++ show productsForJohn
--- examples/embedded.hs
+++ examples/embedded.hs
@@ -3,7 +3,7 @@ import Control.Monad.IO.Class (liftIO)
 import Database.Groundhog.TH
 import Database.Groundhog.Sqlite
 
-data Company = Company {name :: String, headquarter :: Address, dataCentre :: Address, salesOffice :: Address} deriving (Eq, Show)
+data Company = Company {name :: String, producedSkynetAndTerminator :: (Bool, Bool), headquarter :: Address, dataCentre :: Address, salesOffice :: Address} deriving (Eq, Show)
 data Address = Address {city :: String, zipCode :: String, street :: String} deriving (Eq, Show)
 
 mkPersist suffixNamingStyle [groundhog|
@@ -12,22 +12,28 @@ definitions:
     constructors:
       - name: Company
         fields:
-                                        # Property embeddedType of headquarter field is not mentioned, so the corresponding table columns will have names prefixed with headquarter (headquarter$city, headquarter$zip_code, headquarter$street)
-          - name: dataCentre
+          - name: producedSkynetAndTerminator
+            embeddedType:
+              - name: val0
+                dbName: producedSkynet
+              - name: val1
+                dbName: producedTerminator
+          - name: headquarter
             embeddedType:               # If a field has an embedded type you can access its subfields. If you do it, the database columns will match with the embedded dbNames (no prefixing).
               - name: city              # Just a regular list of fields. However, note that you should use default dbNames of embedded
-                dbName: dc_city
+                dbName: hq_city
               - name: zip_code          # Here we use embedded dbName (zip_code) which differs from the name used in Address definition (zipCode) for accessing the field.
-                dbName: dc_zipcode
+                dbName: hq_zipcode
               - name: street
-                dbName: dc_street
-          - name: salesOffice
+                dbName: hq_street
+          - name: dataCentre
             embeddedType:               # Similar declaration, but using another syntax for YAML objects
-              - {name: city, dbName: sales_city}
-              - {name: zip_code, dbName: sales_zipcode}
-              - {name: street, dbName: sales_street}
+              - {name: city, dbName: dc_city}
+              - {name: zip_code, dbName: dc_zipcode}
+              - {name: street, dbName: dc_street}
+                                        # Property embeddedType of salesOffice field is not mentioned, so the corresponding table columns will have names prefixed with salesOffice (salesOffice#city, salesOffice#zip_code, salesOffice#street)
   - embedded: Address
-    dbName: Address                     # This name is used only to set polymorphic part of name of its container. E.g, persistName (a :: SomeData Address) = "SomeData$Address"
+    dbName: Address                     # This name is used only to set polymorphic part of name of its container. E.g, persistName (a :: SomeData Address) = "SomeData#Address"
     fields:                             # The syntax is the same as for constructor fields. Nested embedded types are allowed.
       - name: city                      # This line does nothing and can be omitted. Default settings for city are not changed.
       - name: zipCode
@@ -37,16 +43,18 @@ definitions:
  |]
 
 main = withSqliteConn ":memory:" $ runSqliteConn $ do
-  let commonAddress = Address "Sunnyvale" "18144" "El Camino Real"
-  let company = Company "Cyberdyne Systems" commonAddress commonAddress commonAddress
+  let address = Address "Sunnyvale" "18144" "El Camino Real"
+  let company = Company "Cyberdyne Systems" (False, False) address address address
   runMigration defaultMigrationLogger $ migrate company
-  insert company
+  k <- insert company
   -- compare embedded data fields as a whole and compare their subfields individually
   select (DataCentreField ==. HeadquarterField &&. DataCentreField ~> ZipCodeSelector ==. HeadquarterField ~> ZipCodeSelector) [] 0 0 >>= liftIO . print
   -- after the Cyberdyne headquarter was destroyed by John Connor and T-800, the Skynet development was continued by Cyber Research Systems affiliated with Pentagon
   let newAddress = Address "Washington" "20301" "1400 Defense Pentagon"
   -- compare fields with an embedded value as a whole and update embedded field with a value
-  update [NameField =. "Cyber Research Systems", HeadquarterField =. newAddress] (NameField ==. "Cyberdyne Systems" &&. HeadquarterField ==. commonAddress)
+  update [NameField =. "Cyber Research Systems", HeadquarterField =. newAddress] (NameField ==. "Cyberdyne Systems" &&. HeadquarterField ==. address)
   -- update embedded field with another field as a whole. Separate subfields can be accessed individually for update via ~> as in the select above
   update [DataCentreField =. HeadquarterField, SalesOfficeField =. HeadquarterField] (NameField ==. "Cyber Research Systems" &&. HeadquarterField ==. newAddress)
+  -- eventually the skynet was developed. To access the elements of tuple we use predefined selectors. In Tuple2_0Selector 2 is arity of the tuple, 0 is number of element in it
+  update [ProducedSkynetAndTerminatorField ~> Tuple2_0Selector =. True] (AutoKeyField ==. k)
   select (HeadquarterField ~> ZipCodeSelector ==. "20301") [] 0 0 >>= liftIO . print
