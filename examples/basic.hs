7c7fafd861935ca7b74b7e72f1b0d465c5b9539c
Boris Lykah <lykahb@gmail.com>
2013-05-16 12:51:34 +0300

    use key instead of storing entity directly

--- examples/basic.hs
+++ examples/basic.hs
@@ -1,38 +1,36 @@
-{-# LANGUAGE GADTs, TypeFamilies, TemplateHaskell, QuasiQuotes, FlexibleInstances #-}
+{-# LANGUAGE GADTs, TypeFamilies, TemplateHaskell, QuasiQuotes, FlexibleInstances, StandaloneDeriving #-}
 import Control.Monad.IO.Class (liftIO)
 import Database.Groundhog.TH
 import Database.Groundhog.Sqlite
 
-data Customer a = Customer {customerName :: String, remark :: a} deriving Show
-data Product = Product {productName :: String, quantity :: Int, customer :: Customer String} deriving Show
+data Customer = Customer {customerName :: String, phone :: String} deriving Show
+data Product = Product {productName :: String, quantity :: Int, customer :: DefaultKey Customer}
+deriving instance Show Product
 
--- Code generator will derive the necessary instances and generate other boilerplate code for the entities defined below.
--- It will also create function migrateAll that migrates schema for all non-polymorphic entities 
-mkPersist (defaultCodegenConfig {migrationFunction = Just "migrateAll"}) [groundhog|
-- entity: Customer
+mkPersist defaultCodegenConfig [groundhog|
+- entity: Customer               # Name of the datatype
   constructors:
     - name: Customer
+      fields:
+        - name: customerName
+          dbName: name           # Set column name to "name" instead of "customerName"
       uniques:
         - name: NameConstraint
-          fields: [customerName]
+          fields: [customerName] # Inline format of list
 - entity: Product
 |]
 
-main = withSqliteConn ":memory:" $ runDbConn $ do
-  -- Customer is also migrated because Product references it.
-  -- It is possible to migrate schema for given type, e.g. migrate (undefined :: Customer String), or run migrateAll
-  runMigration defaultMigrationLogger migrateAll
-  let john = Customer "John Doe" "Phone: 01234567"
-  johnKey <- insert john
-  -- John is inserted only once because of the name constraint
-  insert $ Product "Apples" 5 john
-  insert $ Product "Melon" 2 john
-  -- Groundhog prevents SQL injections. Quotes and other special symbols are safe.
-  insert $ Product "Melon" 6 (Customer "Jack Smith" "Don't let him pay by check")
+main = withSqliteConn "mydb.sqlite" $ runDbConn $ do
+  runMigration defaultMigrationLogger $ do
+    migrate (undefined :: Customer)
+    migrate (undefined :: Product)
+  johnKey <- insert $ Customer "John Doe" "0123456789"
+  get johnKey >>= liftIO . print
+  insert $ Product "Apples" 5 johnKey
+  insert $ Product "Melon" 3 johnKey
+  janeKey <- insert $ Customer "Jane Doe" "987654321"
+  insert $ Product "Oranges" 4 janeKey
   -- bonus melon for all large melon orders. The values used in expressions should have known type, so literal 5 is annotated.
   update [QuantityField =. toArith QuantityField + 1] (ProductNameField ==. "Melon" &&. QuantityField >. (5 :: Int))
-  productsForJohn <- select $ CustomerField ==. johnKey
+  productsForJohn <- select $ (CustomerField ==. johnKey) `orderBy` [Asc ProductNameField]
   liftIO $ putStrLn $ "Products for John: " ++ show productsForJohn
-  -- check bonus
-  melon <- select $ (ProductNameField ==. "Melon") `orderBy` [Desc QuantityField]
-  liftIO $ putStrLn $ "Melon orders: " ++ show melon
