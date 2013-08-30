99dbaf9e63478d49b208d3f9f828974e9597946f
Boris Lykah <lykahb@gmail.com>
2013-08-30 22:02:11 +0300

    update bench

--- bench/dbbench.hs
+++ bench/dbbench.hs
@@ -2,7 +2,7 @@
 
 import Control.Monad
 import Database.Groundhog.TH
-import Database.Groundhog.Postgresql
+import Database.Groundhog.Sqlite
 
 data Person = Person {name :: String, age :: Int, height :: Int} deriving (Eq, Show)
 
@@ -12,11 +12,11 @@ mkPersist defaultCodegenConfig [groundhog|
 
 -- First, normal build, then profiling build with "-osuf p_o -hisuf p_hi"
 main :: IO ()
---main = withSqliteConn ":memory:" $ runSqliteConn $ do
-main = withPostgresqlConn "dbname=test user=test password=test host=localhost" $ runPostgresqlConn $ do
+main = withSqliteConn ":memory:" $ runDbConn $ do
+--main = withPostgresqlConn "dbname=test user=test password=test host=localhost" $ runPostgresqlConn $ do
   runMigration silentMigrationLogger $ migrate (undefined :: Person)
   let person = Person "abc" 22 180
   k <- insert $ person
---  replicateM_ 1000000 $ get k --4.3
-  replicateM_ 10000 $ select ((AutoKeyField `asTypeOf` (undefined :: f Person PersonConstructor)) ==. k) [] 0 0 --9.6
+  replicateM_ 100000 $ get k --4.3
+--  replicateM_ 10000 $ select $ AgeField ==. (22 :: Int)
 --  replicateM_ 100000 $ insert person
