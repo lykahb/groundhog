7e1903b56f64bd0263641ec93e50305406232a3f
Boris Lykah <lykahb@gmail.com>
2014-02-11 22:12:58 -0500

    fix mysql init

--- groundhog-test/init_mysql.sql
+++ groundhog-test/init_mysql.sql
@@ -1,5 +1,5 @@
 create database if not exists test;
 use test;
-create user test identified by 'test';
-grant all privileges on *.* to 'test'@'%' with grant option;
+create user 'test' identified by 'test';
+grant all privileges on *.* to 'test'@'localhost' with grant option;
 flush privileges;
