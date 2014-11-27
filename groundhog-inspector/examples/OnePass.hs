01333e95e849ce96043adf5e0e1d215e68c9d2bb
Boris Lykah <lykahb@gmail.com>
2014-11-27 17:06:58 -0500

    groundhog-introspector examples

--- groundhog-inspector/examples/OnePass.hs
+++ groundhog-inspector/examples/OnePass.hs
@@ -12,15 +12,18 @@ do
     mapM_ (\s -> executeRaw False s []) [
       "CREATE TABLE customer (id INTEGER PRIMARY KEY NOT NULL, name VARCHAR NOT NULL, phone VARCHAR NOT NULL)",
       "CREATE TABLE booking (id INTEGER PRIMARY KEY NOT NULL, details VARCHAR, customer INTEGER NOT NULL, FOREIGN KEY(customer) REFERENCES customer(id))"]
-    ts <- collectTables (const True) Nothing
+    tables <- collectTables (const True) Nothing
+    -- Generate datatype declarations
     let dataConfig = defaultDataCodegenConfig {mkType = sqliteMkType, generateUniqueKeysPhantoms = False}
-        decs = generateData dataConfig defaultReverseNamingStyle ts
-    mappingSettings <- generateMapping defaultReverseNamingStyle ts
+        decs = generateData dataConfig defaultReverseNamingStyle tables
+    -- Generate mapping configuration that has tables and column names and other database information
+    mappingSettings <- generateMapping defaultReverseNamingStyle tables
     return (decs, mappingSettings)
   -- Function 'mkPersist' cannot be used here because it tries to reify datatypes which don't exist yet.
   -- So we manually combine declaration with mapping settings to generate mapping declarations
   let mkThEntity dec mapping = applyEntitySettings suffixNamingStyle mapping $ mkTHEntityDef suffixNamingStyle $ fst dec
       thEntities = Map.elems $ Map.intersectionWith mkThEntity decs mappingSettings
+  -- Use mapping configuration to generate mapping declarations
   mappingDecs <- defaultMkEntityDecs thEntities
   migrateFunction <- mkMigrateFunction "migrateAll" thEntities
   return $ concat (map (uncurry (:)) $ Map.elems decs) ++ mappingDecs ++ migrateFunction
---
+++ groundhog-inspector/examples/PrintMappings.hs
@@ -0,0 +1,27 @@
+import Database.Groundhog.Inspector
+import Database.Groundhog.Sqlite
+import Database.Groundhog.TH
+
+import Control.Monad.Trans (liftIO)
+import qualified Data.Map as Map
+import Data.List (isSuffixOf)
+
+import qualified Data.ByteString.Lazy.Char8 as B
+
+main :: IO ()
+main = withSqliteConn ":memory:" $ runDbConn $ do
+	-- Initialize schema
+  mapM_ (\s -> executeRaw False s []) [
+    "CREATE TABLE customer (id INTEGER PRIMARY KEY NOT NULL, name VARCHAR NOT NULL, phone VARCHAR NOT NULL)",
+    "CREATE TABLE booking (id INTEGER PRIMARY KEY NOT NULL, details VARCHAR, customer INTEGER NOT NULL, FOREIGN KEY(customer) REFERENCES customer(id))"]
+  -- Collect tables for further analysis
+  tables <- collectTables (\(_, name) -> not $ "_not_mapped" `isSuffixOf` name) Nothing
+  -- Analyze tables
+  let decs = generateData defaultDataCodegenConfig defaultReverseNamingStyle tables
+  mappings <- generateMapping defaultReverseNamingStyle tables
+  -- Print datatype declarations
+  liftIO $ mapM_ (putStrLn . showData) $ concat $ map (uncurry (:)) $ Map.elems $ decs
+  -- Remove parts of mapping that are defaults for the chosen naming style
+  let mappings' = Map.intersectionWith (minimizeMapping suffixNamingStyle . fst) decs mappings
+  -- Print mappings
+  liftIO $ B.putStrLn $ showMappings $ Map.elems mappings'
