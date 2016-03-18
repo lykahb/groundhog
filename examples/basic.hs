6d2f68129109330e6e0d0c3d98f03e791ee7a741
Boris Lykah <lykahb@gmail.com>
2016-03-18 16:37:18 -0400

    more detailed descriptions for the basic example

--- examples/basic.hs
+++ examples/basic.hs
@@ -18,18 +18,22 @@ mkPersist defaultCodegenConfig [groundhog|
 |]
 
 main = withSqliteConn ":memory:" $ runDbConn $ do
+  -- create tables
   runMigration $ do
     migrate (undefined :: Customer)
     migrate (undefined :: Product)
   johnKey <- insert $ Customer "John Doe" "0123456789"
+  janeKey <- insert $ Customer "Jane Doe" "987654321"
+  -- get customer by its primary key
   get johnKey >>= liftIO . print
+  -- insert objects with a foreign key
   insert $ Product "Apples" 5 johnKey
   insert $ Product "Melon" 3 johnKey
-  janeKey <- insert $ Customer "Jane Doe" "987654321"
+  insert $ Product "Oranges" 4 janeKey
+  -- select all customers without condition
   allCustomers <- select CondEmpty
   liftIO $ putStrLn $ "All customers: " ++ show (allCustomers :: [Customer])
-  insert $ Product "Oranges" 4 janeKey
-  -- bonus melon for all large melon orders. The values used in expressions should have known type, so literal 5 is annotated.
+  -- bonus melon for all large melon orders. The values used in expressions should have defined type, so literal 5 is annotated.
   update [QuantityField =. liftExpr QuantityField + 1] (ProductNameField ==. "Melon" &&. QuantityField >. (5 :: Int))
   productsForJohn <- select $ (CustomerField ==. johnKey) `orderBy` [Asc ProductNameField]
   liftIO $ putStrLn $ "Products for John: " ++ show productsForJohn
